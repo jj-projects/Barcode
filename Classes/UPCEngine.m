274d07789a25e8b9e548f53eb1d3295793b31d4a
Jörg Jünger <joerg@jj-projects.de>
2013-01-23 12:52:16 +0100

    add first implementation of UPC decoding

--- Classes/UPCEngine.m
+++ Classes/UPCEngine.m
@@ -7,6 +7,9 @@
 //
 
 #import "UPCEngine.h"
+#import "ZBarImage.h"
+#import "ZBarSymbol.h"
+#import "ZBarImageScanner.h"
 
 
 @implementation UPCEngine
@@ -25,7 +28,55 @@
 - (void)decode:(NSDictionary *)dictionary {
 	NSLog(@"decode:");
    
+   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	UIImage *image = (UIImage *)[dictionary objectForKey:@"image"];
+	
+	NSMutableArray *messages = [NSMutableArray array];
+	
+	
+	// Create gray image.
+	CGImageRef grayImage = [self grayImageFromImage:image withRect:(CGRect)[(NSValue *)[dictionary objectForKey:@"rect"] CGRectValue]];
+	if(grayImage == NULL) {
+		if(!self.cancel)
+			[self performSelectorOnMainThread:@selector(didNotDecode:) withObject:[NSDictionary dictionaryWithObjectsAndKeys:image, @"image", nil] waitUntilDone:NO];
+		[self performSelectorOnMainThread:@selector(didStopOperation) withObject:nil waitUntilDone:NO];
+		[pool release];
+		return;
+	}
+	
+	if(self.cancel) {
+		CGImageRelease(grayImage);
+		[self performSelectorOnMainThread:@selector(didStopOperation) withObject:nil waitUntilDone:NO];
+		[pool release];
+		return;
+	}   
+
+   ZBarImage * zImage = [[[ZBarImage alloc] initWithCGImage:grayImage] autorelease];
+   assert(NULL != zImage);
    
+   ZBarImageScanner *scanner = [[[ZBarImageScanner alloc] init] autorelease];
+   assert(NULL != scanner);
+   
+   [scanner scanImage:zImage];
+   ZBarSymbolSet *result = [scanner results];
+   
+   NSString *message = [[NSString alloc] initWithData:[result data] encoding:NSUTF8StringEncoding];
+   assert(NULL != message);
+   [messages addObject:message];
+
+   if([messages count] > 0) {
+		if(!self.cancel)
+			[self performSelectorOnMainThread:@selector(didDecode:) withObject:[NSDictionary dictionaryWithObjectsAndKeys:image, @"image", [messages objectAtIndex:0], @"string", nil] waitUntilDone:NO];
+	} else {
+		if(!self.cancel)
+			[self performSelectorOnMainThread:@selector(didNotDecode:) withObject:[NSDictionary dictionaryWithObjectsAndKeys:image, @"image", nil] waitUntilDone:NO];
+	}
+	
+	[self performSelectorOnMainThread:@selector(didStopOperation) withObject:nil waitUntilDone:NO];
+	
+	[pool release];
+
 }
 
 
